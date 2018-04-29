5cc24684b3fe5e83698187026a1e90540cdfdb94
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-04-28 21:17:29 -0700

    Add my-tweets feature

--- liri.js
+++ liri.js
@@ -1,17 +1,29 @@
 require("dotenv").config();
 const keys = require('./keys');
 
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 const arg = process.argv[2];
 
-if(arg === 'my-tweets'){
 
+
+
+
+if(arg === 'my-tweets'){
+    var params = {screen_name: 'g3642563', count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets.map(x => `${x.text} on ${x.created_at.split(' ')[0]} at ${x.created_at.split(' ')[3]}`));
+      }
+    });
 } else if(arg === 'spotify-this-song'){
 
 } else if(arg === 'movie-this'){
 
 } else if(arg === 'do-what-it-says'){
-    
+
 }
